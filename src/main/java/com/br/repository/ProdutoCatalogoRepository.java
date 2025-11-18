a5e268ea022a1914c720805c60393d951b4d5229
Edivan1000 <edivanprofissional10@gmail.com>
2025-11-18 20:44:12 -0300

    Delete src directory

--- src/main/java/com/br/repository/ProdutoCatalogoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.repository;
-
-import com.br.model.ProdutoCatalogo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoCatalogoRepository extends JpaRepository<ProdutoCatalogo, Long> {
-}
